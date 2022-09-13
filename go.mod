8151388862dbb1e81db2a7d0b75f475f15f1355d
Brayan-BH <brayangithub007@gmail.com>
2022-09-12 22:04:50 -0500

    Upload Files

---
+++ go.mod
@@ -0,0 +1,6 @@
+module generateHash
+
+go 1.8
+
+require golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 
+// indirect
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+func main() {
+	password := []byte("sape123")
+	hashPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(hashPassword))
+
+	err = bcrypt.CompareHashAndPassword(hashPassword, password)
+	fmt.Println(err)
+}
